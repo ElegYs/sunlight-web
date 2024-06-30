6710402ef08916dc36bc0a38207ccac5c1f7b0b5
DoubleTrouble-rbxm <ldanilka18@gmail.com>
2024-06-30 23:48:57 +0300

    Developing...

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
+  logo: <span>SunLight</span>,
   chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
+    link: 'https://discord.gg/4rH2DuYYEN',
   },
 }
 
